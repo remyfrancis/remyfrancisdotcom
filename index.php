28362eaa6da637cb3d486d96b4c8f06f53636fee
Remy Francis <remy.e.francis@gmail.com>
2018-11-30 16:29:00 -0400

    Hero image finally fixed completely

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <!-- Header -->
 <?php include('header.php'); ?>
 
-<!--Main Navigation-->
-<div class="view intro hm-black-slight jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url('img/2.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
-    <div class="full-bg-img">
-        <div class="container">
-            <div class="d-flex align-items-center d-flex justify-content-center" style="height: 700px">
-                <div class="row mt-5">
-                    <div class="col-md-12 wow zoomIn mb-3 mx-3">
-                        <div class="intro-info-content text-center">
-                            <h1 class="display-3 mb-2 white-text wow animated fadeInDown delay-1s">REMY <a class="white-text font-weight-bold">FRANCIS</a></h1>
-                            <h5 class="font-up mb-3 mt-1 white-text font-bold wow animated fadeInDown" data-wow-delay="0.4s">Full Stack Web & Mobile Developer</h5>
-                            <h5 class="mb-3 mt-1 white-text font-bold wow animated fadeInDown" data-wow-delay="0.5s">I develop awesome cross-platform mobile web apps and websites.</h5>
-                            <a class="btn btn-success waves-effect btn-lg btn-rounded font-weight-bold wow animated fadeInDown" data-wow-delay="0.6s" href="http://www.remyfrancis.com/proposal.html">Here's What I Do</a> 
-                            <h5 class="display-4 white-text animated bounce infinite"><i class="fas fa-angle-down"></i></h5>
-                        </div>
+<!-- Intro Section -->
+<div id="home" class="view jarallax" data-jarallax='{"speed": 0.2}' style="background-image: url('img/christmas.jpg'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+    <div class="mask rgba-black-light">
+        <div class="container h-100 d-flex justify-content-center align-items-center">
+            <div class="row smooth-scroll">
+                <div class="col-md-12 pt-3">
+                    <div class="white-text text-center pt-5">
+                    <h1 class="display-3 mb-2 white-text wow animated fadeInDown delay-1s">REMY <a class="white-text font-weight-bold">FRANCIS</a></h1>
+                    <h5 class="font-up mb-3 mt-1 white-text font-bold wow animated fadeInDown" data-wow-delay="0.4s">Full Stack Web & Mobile Developer</h5>
+                    <h5 class="mb-3 mt-1 white-text font-bold wow animated fadeInDown" data-wow-delay="0.4s">I develop awesome cross-platform mobile web apps and websites.</h5>
+                    <a class="btn btn-success waves-effect btn-lg btn-rounded font-weight-bold wow animated fadeInDown" data-wow-delay="0.6s" href="http://www.remyfrancis.com/proposal.html">50% Off Select Layouts Until December 31st</a>
                     </div>
                 </div>
             </div>
